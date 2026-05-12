d76ea17eca39284635b47787221d7c38d108b664
Emilkazxxzxc <emily.skalny@gmail.com>
2026-05-12 23:28:32 +0200

    Add files via upload

--- swetrki.h
+++ swetrki.h
@@ -5,11 +5,28 @@ using std::vector;
 using std::string;
 
 class Film{
-public:
     string title;
     string keywords;
     vector<Review> Reviews;
-    bool AddReview(Review& r);
+    bool AddReview(Review& r){
+        Reviews.push_back(r);
+        return true;
+    };
+    bool RemoveReview(Review& r){
+        for(int i=0;i<Reviews.size();i++){
+            if(Reviews[i].autor==r.autor){
+                Reviews.erase(Reviews.begin()+i);
+                return true;
+            }
+        }
+        return false;
+    };
+    bool ViewReviews(){
+        for(int i=0;i<Reviews.size();i++){
+            cout<<Reviews[i].autor<<" "<<Reviews[i].rank<<" "<<Reviews[i].text<<endl;
+        }
+        return true;
+    };
 };
 class Review{
 public:
@@ -17,6 +34,7 @@ public:
     string text;
     string autor;
 };
+
 class user{
 public:
     string login;
