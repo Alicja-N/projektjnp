a0bf478f00beec68f9b32d330f9399d1553d616d
Alicja-N <alicja.niec@gmail.com>
2026-05-19 23:32:08 +0200

    Add files via upload

--- user.cpp
+++ user.cpp
@@ -1,17 +1,52 @@
 #include "swetrki.h"
 #include <iostream>
+#include <vector>
+
 using namespace std;
-class User{
-public:
-    string login;
-    string name;
-    bool isAuthenticated;
-    bool isAdmin;
-    
-    bool isUserAuthenticated() const {
-        return isAuthenticated;
+
+
+void User::loginUser() {
+    string inputLogin;
+    cout << "Podaj login: ";
+    cin >> inputLogin;
+
+    //Szukamy w administratorach
+    for (const User& admin : RegisteredAdmins) {
+        if (admin.login == inputLogin) {
+            this->login = admin.login;
+            this->name = admin.name;
+            this->isAuthenticated = true;
+            this->isAdmin = true;
+            cout << "Zalogowano pomyślnie jako Administrator: " << this->name << endl;
+            return;
+        }
     }
-    void loginUser(string login, string password);
-    bool checkAdmin() const;
-    string getLogin() const;
-};
+
+    //Szukamy w zwykłych użytkownikach
+    for (const User& user : RegisteredUsers) {
+        if (user.login == inputLogin) {
+            this->login = user.login;
+            this->name = user.name;
+            this->isAuthenticated = true;
+            this->isAdmin = false;
+            cout << "Zalogowano pomyślnie jako Użytkownik: " << this->name << endl;
+            return;
+        }
+    }
+
+    //Jeśli pętla nie znalazła loginu
+    this->isAuthenticated = false;
+    this->isAdmin = false;
+    cout << "Błąd logowania: Użytkownik o podanym loginie nie istnieje." << endl;
+}
+
+bool User::checkAdmin() const {
+    return isAuthenticated && isAdmin;
+}
+
+string User::getLogin() const {
+    if (isAuthenticated) {
+        return login;
+    }
+    return "Niezalogowany";
+}
